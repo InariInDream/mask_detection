eb4122ebaa3379ef41d04a14ed56ce92d783a34c
梦里的伏见 <inariindream@163.com>
2023-01-03 22:30:17 +0800

    Update face_detect_main.m

--- face_detect_main.m
+++ face_detect_main.m
@@ -1,26 +1,37 @@
 clc,clear,close all  % 清理命令区、清理工作区、关闭显示图形
 warning off       % 消除警告
-feature jit off      % 加速代码运行
-tic
+feature jit off      % 关闭即时编译，加快运行速度
+tic         % 开始计时
 [filename ,pathname]=...
     uigetfile({'*.*';'*.jpg';'*.png';},'选择图片'); %选择图片路径
-str=[pathname filename]; % 合成路径+文件名
-if str %如果取消选择，启动视频
-    img = imread(str);        % 读图
+% 合成路径+文件名
+str=[pathname filename]; 
+%如果取消选择，启动视频
+if str
+     % 读图
+    img = imread(str);       
     image_process(img,'picture');
 else
-    cam=webcam(1);%读取图片
+    %读取图片
+    cam=webcam(1);
     img=snapshot(cam);
+    %显示图片
     preview(cam)
-    img = snapshot(cam);    % Get a single frame
-    figure,image(img);      % Display the frame in a figure window.
-    num_frame=3;
+    % 使用snapshot函数获取图像
+    img = snapshot(cam);    
+    % 显示图像
+    figure,image(img);
+    %设置帧数
+    num_frame=1;
     % 获取num_frame个帧
     for idx = 1:num_frame
+        %循环获取num_frame个帧
         figure;
-        img = snapshot(cam);%获取每一帧图片
+        %获取每一帧图片
+        img = snapshot(cam);
         image_process(img,"video");
-        pause(3)%每隔五秒处理一次
+        %每隔3秒处理一次
+        pause(3)
     end
     clear cam
 end
