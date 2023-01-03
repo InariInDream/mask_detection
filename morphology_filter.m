cae5411f14671d32fa3ec35df51b805f3048aa94
梦里的伏见 <inariindream@163.com>
2023-01-03 22:32:02 +0800

    Update morphology_filter.m

--- morphology_filter.m
+++ morphology_filter.m
@@ -2,7 +2,7 @@ function Iobrcbr = morphology_filter(im,sca)
 % 形态学滤波器
 %函数输入: 
 %         im:   输入的二维图像矩阵
-%         sca : 结构元素尺寸代销
+%         sca : 结构元素尺寸
 %函数输出: 
 %         Iobrcbr: 形态学滤波图像
 % 形态学滤波
@@ -12,7 +12,7 @@ Io=imopen(im,se);              % 开操作，内部细小连接去除
 Ioc=imclose(Io,se);            % 闭操作，内部小凹陷去除
 Iobr=imreconstruct(Io,Ioc);    % 结构重建
 
-Iobrd = imdilate(Iobr,se);   % 膨胀
+Iobrd = imdilate(Iobr,se);   % 膨胀操作，去除小孔洞
 
 Iobrcbr = imreconstruct(imcomplement(Iobrd),imcomplement(Iobr)); % 结构重建
-Iobrcbr = imcomplement(Iobrcbr);   
+Iobrcbr = imcomplement(Iobrcbr);                                % 取反
